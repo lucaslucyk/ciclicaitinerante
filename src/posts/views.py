d3a0b85b7647edbe4fed3825a537cf8d3a2e5541
Lucas Lucyk <lucaslucyk@gmail.com>
2019-06-09 18:27:37 -0300

    CORRECCION MSGS

--- src/posts/views.py
+++ src/posts/views.py
@@ -23,166 +23,171 @@ from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-	if not request.user.is_staff or not request.user.is_superuser:
-		raise Http404
-		
-	form = PostForm(request.POST or None, request.FILES or None)
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+        
+    form = PostForm(request.POST or None, request.FILES or None)
 
-	if form.is_valid():
-		instance      = form.save(commit=False)
-		instance.user = request.user
-		instance.save()
-		# message success
-		messages.success(request, "Successfully Created")
-		return HttpResponseRedirect(instance.get_absolute_url())
-	
-	context = {
-		"form": form,
-	}
-	return render(request, "post_form.html", context)
+    if form.is_valid():
+        instance      = form.save(commit=False)
+        instance.user = request.user
+        instance.save()
+        # message success
+        messages.success(request, "Creación correcta!")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 '''
-Created for Django Code Review
+ Created for Django Code Review
 '''
 
 from django.views.generic import DetailView
 
 class PostDetailView(DetailView):
-	template_name = 'post_detail.html' 
-	
-	def get_object(self, *args, **kwargs):
-		slug    = self.kwargs.get("slug")
-		instance= get_object_or_404(Post, slug=slug)
-		
-		if instance.publish > timezone.now().date() or instance.draft:
-			if not self.request.user.is_staff or not self.request.user.is_superuser:
-				raise Http404
-		return instance
-	
-	def get_context_data(self, *args, **kwargs):
-		context                = super(PostDetailView, self).get_context_data(*args, **kwargs)
-		instance               = context['object']
-		context['share_string']= quote_plus(instance.content)
+    template_name = 'post_detail.html' 
+    
+    def get_object(self, *args, **kwargs):
+        slug    = self.kwargs.get("slug")
+        instance= get_object_or_404(Post, slug=slug)
+        
+        if instance.publish > timezone.now().date() or instance.draft:
+            if not self.request.user.is_staff or not self.request.user.is_superuser:
+                raise Http404
+        return instance
+    
+    def get_context_data(self, *args, **kwargs):
+        context                = super(PostDetailView, self).get_context_data(*args, **kwargs)
+        instance               = context['object']
+        context['share_string']= quote_plus(instance.content)
 
-		return context
-	
+        return context
+    
 # in urls.py --> PostDetailView.as_view() instead of post_detail
 
 
 def post_detail(request, slug=None):
-	instance = get_object_or_404(Post, slug=slug)
-	if instance.publish > timezone.now().date() or instance.draft:
-		if not request.user.is_staff or not request.user.is_superuser:
-			raise Http404
+    #instance = get_object_or_404(Post, slug=slug)
+    try:
+        instance = Post.objects.get(slug=slug)
+    except:
+        raise Http404
 
-	share_string      = quote_plus(instance.content)
-	initial_data      = {
-		"content_type": instance.get_content_type,
-		"object_id"   : instance.id,
-	}
+    if instance.publish > timezone.now().date() or instance.draft:
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
 
-	form = CommentForm(request.POST or None, initial=initial_data)
+    share_string      = quote_plus(instance.content)
+    initial_data      = {
+        "content_type": instance.get_content_type,
+        "object_id"   : instance.id,
+    }
 
-	if form.is_valid():
-		c_type      = form.cleaned_data.get("content_type")
-		content_type= ContentType.objects.get(model=c_type)
-		obj_id      = form.cleaned_data.get("object_id")
-		content_data= form.cleaned_data.get("content")
-		parent_obj  = None
+    form = CommentForm(request.POST or None, initial=initial_data)
 
-		try:
-			parent_id = int(request.POST.get("parent_id"))
-		except:
-			parent_id = None
+    if form.is_valid():
+        c_type      = form.cleaned_data.get("content_type")
+        content_type= ContentType.objects.get(model=c_type)
+        obj_id      = form.cleaned_data.get("object_id")
+        content_data= form.cleaned_data.get("content")
+        parent_obj  = None
 
-		if parent_id:
-			parent_qs = Comment.objects.filter(id=parent_id)
-			if parent_qs.exists() and parent_qs.count() == 1:
-				parent_obj = parent_qs.first()
+        try:
+            parent_id = int(request.POST.get("parent_id"))
+        except:
+            parent_id = None
 
-		new_comment, created = Comment.objects.get_or_create(
-									user        = request.user,
-									content_type= content_type,
-									object_id   = obj_id,
-									content     = content_data,
-									parent      = parent_obj
-								)
-		return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
+        if parent_id:
+            parent_qs = Comment.objects.filter(id=parent_id)
+            if parent_qs.exists() and parent_qs.count() == 1:
+                parent_obj = parent_qs.first()
 
-	comments = instance.comments #Comment.objects.filter_by_instance(instance)
+        new_comment, created = Comment.objects.get_or_create(
+                                    user        = request.user,
+                                    content_type= content_type,
+                                    object_id   = obj_id,
+                                    content     = content_data,
+                                    parent      = parent_obj
+                                )
+        return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
 
-	context = {
-		"title"       : instance.title,
-		"object"      : instance,
-		"share_string": share_string,
-		"comments"    : comments,
-		"comment_form": form,
-	}
-	return render(request, "post_detail.html", context)
+    comments = instance.comments #Comment.objects.filter_by_instance(instance)
+
+    context = {
+        "title"       : instance.title,
+        "object"      : instance,
+        "share_string": share_string,
+        "comments"    : comments,
+        "comment_form": form,
+    }
+    return render(request, "post_detail.html", context)
 
 def post_list(request):
-	today        = timezone.now().date()
-	queryset_list= Post.objects.active() #.order_by("-timestamp")
+    today        = timezone.now().date()
+    queryset_list= Post.objects.active() #.order_by("-timestamp")
 
-	if request.user.is_staff or request.user.is_superuser:
-		queryset_list = Post.objects.all()
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
 
-	query = request.GET.get("q")
-	if query:
-		queryset_list = queryset_list.filter(
-				Q(title__icontains=query)|
-				Q(content__icontains=query)|
-				Q(user__first_name__icontains=query) |
-				Q(user__last_name__icontains=query)
-				).distinct()
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(
+                Q(title__icontains=query)|
+                Q(content__icontains=query)|
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+                ).distinct()
 
-	paginator       = Paginator(queryset_list, 8) # Show 25 contacts per page
-	page_request_var= "page"
-	page            = request.GET.get(page_request_var)
+    paginator       = Paginator(queryset_list, 8) # Show 25 contacts per page
+    page_request_var= "page"
+    page            = request.GET.get(page_request_var)
 
-	try:
-		queryset = paginator.page(page)
-	except PageNotAnInteger:
-		# If page is not an integer, deliver first page.
-		queryset = paginator.page(1)
-	except EmptyPage:
-		# If page is out of range (e.g. 9999), deliver last page of results.
-		queryset = paginator.page(paginator.num_pages)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
 
 
-	context = {
-		"object_list"     : queryset, 
-		"title"           : "Blog cíclica itinerante",
-		"page_request_var": page_request_var,
-		"today"           : today,
-	}
-	return render(request, "post_list.html", context)
+    context = {
+        "object_list"     : queryset, 
+        "title"           : "Blog cíclica itinerante",
+        "page_request_var": page_request_var,
+        "today"           : today,
+    }
+    return render(request, "post_list.html", context)
 
 def post_update(request, slug=None):
-	if not request.user.is_staff or not request.user.is_superuser:
-		raise Http404
-	instance= get_object_or_404(Post, slug=slug)
-	form    = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance= get_object_or_404(Post, slug=slug)
+    form    = PostForm(request.POST or None, request.FILES or None, instance=instance)
 
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
-		return HttpResponseRedirect(instance.get_absolute_url())
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Se actualizó el siguiente <a href='#' class='alert-link'>Ítem</a>", extra_tags='html_safe')
+        return HttpResponseRedirect(instance.get_absolute_url())
 
-	context = {
-		"title"   : instance.title,
-		"instance": instance,
-		"form"    :form,
-	}
-	return render(request, "post_form.html", context)
+    context = {
+        "title"   : instance.title,
+        "instance": instance,
+        "form"    :form,
+    }
+    return render(request, "post_form.html", context)
 
 
 
 def post_delete(request, slug=None):
-	if not request.user.is_staff or not request.user.is_superuser:
-		raise Http404
-	instance = get_object_or_404(Post, slug=slug)
-	instance.delete()
-	messages.success(request, "Eliminación completa")
-	return redirect("posts:list")
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
+    instance.delete()
+    messages.success(request, "Eliminación completa")
+    return redirect("posts:list")
